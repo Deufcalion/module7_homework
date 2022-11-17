561af83d044cbec6694792e2f954634900118369
Deufcalion <SleepGhost89@gmail.com>
2022-11-17 10:52:19 +0300

    fix 7.5

--- lesson7_5(ООП в ES6)/task5.js
+++ lesson7_5(ООП в ES6)/task5.js
@@ -24,18 +24,22 @@ class Gadjet {
   }
     
     class MyGadjet extends Gadjet {
-      constructor (name, brand, battaryCappacity, material){
+      constructor (name, brand, battaryCappacity, material, power){
         super (name, battaryCappacity);
         this.brand = brand;
         this.material = material;
         this.connect = false;
+        this.power = power
       }
     }
   
-  const phone = new MyGadjet ("Телефон", "Samsung", 5000, "metall/plastic");
+  const phone = new MyGadjet ("Телефон", "Samsung", 5000, "metall/plastic", 5);
   phone.unconnected()
   console.log(phone)
   
-  const laptop = new MyGadjet ("Ноутбук", "lenovo", 10000, "plastic");
+  const laptop = new MyGadjet ("Ноутбук", "lenovo", 10000, "plastic", 60);
   laptop.connected()
   console.log(laptop)
+
+  const powerConsumption = phone.power + laptop.power
+  console.log(`Потребляемая мощность включенных в сеть приборов - ${powerConsumption} Ватт`)
